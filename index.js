b52446a518b87680d9a338af1d7f550c43336f77
Phil McGarty <philmcgarty@hotmail.com>
2022-04-17 18:44:43 -0400

    added validation

--- index.js
+++ index.js
@@ -9,48 +9,92 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the project name?'
+        message: 'What is the project name? (required)',
+        validate: titleInput => {
+            if (titleInput){
+                return true;
+            } else {
+                console.log('Please enter the project name');
+                return false;
+            };
+        }
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Enter a description of the project'
+        message: 'Enter a description of the project (required)',
+        validate: descInput => {
+            if (descInput){
+                return true;
+            } else {
+                console.log('Please enter a project description');
+                return false;
+            };
+        }
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Provide installation instructions'
+        message: 'Provide installation instructions (optional)'
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions for use?' // ask about this
+        message: 'Provide instructions for how to use the software? (optional)'
     },
     {
         type: 'list',
         name: 'license',
         message: 'Choose a license type:',
-        choices: ['GPL','Apache','Ms-PL','BSD','CDDL','EPL','MIT'] // ask about this
+        choices: ['GPL','Apache','Ms-PL','BSD','CDDL','EPL','MIT']
     },
     {
         type: 'input',
         name: 'contributors',
-        message: 'Contributors?' //ask about this
+        message: 'Give guidelines for contributors to the project (optional)'
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'Tests?' // ask about this
+        message: 'Provide test data for the project (optional)'
     },
     {
         type: 'input',
         name: 'githubUser',
-        message: 'Enter your GitHub user name'
+        message: 'Enter your GitHub user name (required)',
+        validate: gitInput => {
+            if (gitInput){
+                return true;
+            } else {
+                console.log('Please enter your GitHub user name');
+                return false;
+            };
+        }
     },
     {
         type: 'input',
         name: 'email',
-        message: 'Enter your email address'
+        message: 'Enter your email address (required)',
+        validate: emailInput => {
+            if (emailInput){
+                var atSign =false;
+                [...emailInput].forEach(letter => { // Got the [...emailInput] format from: https://betterprogramming.pub/how-to-iterate-through-strings-in-javascript-65c51bb3ace5
+                     
+                    if (letter === "@"){
+                        atSign = true;
+                    }
+                })
+                if (atSign){
+                    return true;
+                } else {
+                    console.log(' Please enter a valid email');
+                    return false;
+                }
+            } else {
+                console.log('Please enter your email');
+                return false;
+            };
+        }
     }
 ];
 
